6c4b261c4251193381e95c7948b9de2de54a538a
Mourya Pranay <styooodnt@gmail.com>
2025-06-02 23:35:34 +0530

    issue #1 initial commit of repository

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-content">
+        <span>Portfolio</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
