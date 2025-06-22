c31590dedd9bf05d81400dfd5bf1b3f924931c79
Mourya Pranay <styooodnt@gmail.com>
2025-06-22 22:11:58 +0530

    issue #35 fix path for photo and video

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -18,7 +18,7 @@ const Projects = () => {
     {
       key: 'brain-vis1',
       video: true,
-      path: '/portfolio/projects/brain-vis.mp4',
+      path: '/projects/brain-vis.mp4',
       title: 'Brain Visualisation',
       description: `
       Built a web application using React, Vite and Typescript for EEG data visualization in neuroscience research.
